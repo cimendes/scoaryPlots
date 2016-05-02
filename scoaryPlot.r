f678d3e3f4741df6e76ddf19b3b14e5e5d6c60df
cimendes <iines.mendes@gmail.com>
2016-05-02 14:03:38 +0100

    -changes made to the source file -added control plot

--- scoaryPlot.r
+++ scoaryPlot.r
@@ -1,3 +1,8 @@
+if (!require("hexbin")){
+  install.packages("hexbin",dependencies = TRUE)
+  library('hexbin')
+}
+
 #setwd("C:/Users/cimendes/Dropbox/Tese/Scoary_Debug")
 
 #lala=list.files()
@@ -19,10 +24,6 @@ negative_present_in=data.df.sub[,5]
 png(paste(file,"_scatterplot_gray.png"), width=480, height=480)
 
 #create plot
-if (!require("hexbin")){
-  install.packages("hexbin",dependencies = TRUE)
-  library('hexbin')
-}
 
 cols <- colorRampPalette(c("darkorchid4","darkblue","green","yellow", "red"))
 
@@ -35,6 +36,16 @@ dev.off()
 png(paste(file,"_scatterplot_colour.png"), width=480, height=480)
 cols <- colorRampPalette(c("darkorchid4","darkblue","green","yellow", "red"))
 plot(hexbin(negative_present_in,positive_present_in), main = file ,
+     colorcut = seq(0,1,length.out=24),
+     colramp = function(n) cols(24))
+dev.off()
+
+#control - all data
+positive_present_in_alldata=data.df[,4]
+negative_present_in_alldata=data.df[,5]
+png(paste(file,"_alldata.png"), width=480, height=480)
+cols <- colorRampPalette(c("darkorchid4","darkblue","green","yellow", "red"))
+plot(hexbin(negative_present_in_alldata,positive_present_in_alldata), main = file ,
      colorcut = seq(0,1,length.out=24),
      colramp = function(n) cols(24))
 dev.off()
