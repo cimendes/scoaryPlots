66ed1031fba661efa0c1b8f072de7a1de70beead
cimendes <iines.mendes@gmail.com>
2016-05-05 13:52:03 +0100

    -modified source file

--- scoaryPlot.r
+++ scoaryPlot.r
@@ -2,8 +2,18 @@ if (!require("hexbin")){
   install.packages("hexbin",dependencies = TRUE)
   library('hexbin')
 }
-
-#setwd("C:/Users/cimendes/Dropbox/Tese/Scoary_Debug")
+if (!require("tm")){
+  install.packages("tm",dependencies = TRUE)
+  library('tm')
+}
+if (!require("SnowballC")){
+  install.packages("SnowballC",dependencies = TRUE)
+  library('SnowballC')
+}
+if (!require("wordcloud")){
+  install.packages("wordcloud",dependencies = TRUE)
+  library('wordcloud')
+}
 
 #lala=list.files()
 
@@ -25,27 +35,55 @@ png(paste(file,"_scatterplot_gray.png"), width=480, height=480)
 
 #create plot
 
-cols <- colorRampPalette(c("darkorchid4","darkblue","green","yellow", "red"))
+cols <- colorRampPalette(c("darkblue", "red"))
+bANDw <- colorRampPalette(c("lightgray","black"))
 
 #gray scale
 bin<-hexbin(negative_present_in,positive_present_in)
-plot(bin, main=file)
+#cols <- colorRampPalette(c("lightgray","black"))
+plot(bin, main=file,colorcut = seq(0,1,length.out=15),
+     colramp = function(n) bANDw(15))
 dev.off()
 
 #colours
 png(paste(file,"_scatterplot_colour.png"), width=480, height=480)
-cols <- colorRampPalette(c("darkorchid4","darkblue","green","yellow", "red"))
+#cols <- colorRampPalette(c("darkblue","red"))
 plot(hexbin(negative_present_in,positive_present_in), main = file ,
-     colorcut = seq(0,1,length.out=24),
-     colramp = function(n) cols(24))
+     colorcut = seq(0,1,length.out=15),
+     colramp = function(n) cols(15))
 dev.off()
 
 #control - all data
 positive_present_in_alldata=data.df[,4]
 negative_present_in_alldata=data.df[,5]
 png(paste(file,"_alldata.png"), width=480, height=480)
-cols <- colorRampPalette(c("darkorchid4","darkblue","green","yellow", "red"))
+#cols <- colorRampPalette(c("darkblue","red"))
 plot(hexbin(negative_present_in_alldata,positive_present_in_alldata), main = file ,
-     colorcut = seq(0,1,length.out=24),
-     colramp = function(n) cols(24))
+     colorcut = seq(0,1,length.out=15),
+     colramp = function(n) cols(15))
 dev.off()
+
+
+#Exclusive accessory genome - file and wordcloud
+max.Number_pos_present_in=max(positive_present_in)
+subset.exclusive=subset(data.df,Number_pos_present_in==max.Number_pos_present_in & Number_neg_present_in == 0)
+if (length(row.names(subset.exclusive)) != 0){
+  write.table(subset.exclusive, file=paste(file,"exclusive_present.csv"), sep=";", row.names = FALSE)
+  #TODO
+  jeopCorpus <- Corpus(VectorSource(subset.exclusive$Annotation))
+  jeopCorpus <- tm_map(jeopCorpus, PlainTextDocument)
+  jeopCorpus <- tm_map(jeopCorpus, removePunctuation)
+  wordcloud(jeopCorpus, max.words = 100, random.order = FALSE)
+  
+  } else{
+  print(paste("No value to print in ", file, "exclusive present"))
+  }
+max.Number_neg_present_in=max(negative_present_in)
+subset.absent=subset(data.df,Number_pos_present_in==0 & max.Number_neg_present_in == 0)
+if (length(row.names(subset.absent)) != 0){
+  write.table(subset.absent, file=paste(file,"exclusive_absent.csv"), sep=";", row.names = FALSE)
+  } else{
+  
+  print(paste("No value to print in ", file, "exclusive absent"))
+  }
+
